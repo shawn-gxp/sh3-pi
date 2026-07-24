8c60962b78d20400842571188e2aec652bab8190
shawn-gxp <shawn.ac@gxp-technologies-india.com>
2026-07-24 09:41:33 +0530

    refactor: remove legacy fall_detection_pi subfolder in favor of sibling layout

--- fall_detection_pi/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Standalone fall / bed-exit detection package for the SH3 Pi hub.
-
-Install:  pip install -e ./fall_detection_pi
-Or put the *parent* of this directory on PYTHONPATH and: import fall_detection_pi
-"""
-
-__version__ = "0.2.0"
